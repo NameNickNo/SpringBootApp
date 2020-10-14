c0e7032ddd63d3b3446aa8ecc4c242000d31900b
Nickolay <n-deryagin@bk.ru>
2020-10-14 11:39:18 +0300

    Creating project

---
+++ src/main/java/net/proselyte/demo/controller/UserController.java
@@ -0,0 +1,59 @@
+package net.proselyte.demo.controller;
+
+import net.proselyte.demo.model.User;
+import net.proselyte.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String getAll(Model model) {
+        List <User> users = userService.getAll();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping("/create")
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Integer id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("user-update/{id}")
+    public String updateUserForm(@PathVariable ("id") Integer id, Model model) {
+        User user = userService.getById(id);
+        model.addAttribute("user", user);
+        return "user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/net/proselyte/demo/service/UserService.java
@@ -0,0 +1,34 @@
+package net.proselyte.demo.service;
+
+import net.proselyte.demo.model.User;
+import net.proselyte.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getById(Integer id) {
+        return userRepository.getOne(id);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
